68f7b02dd1161ceca03de98e34f5763873430f4d
Mark Morgan <markmorgan.mcp@gmail.com>
2021-04-02 17:51:34 +0100

    feat: js for toggling hamburger menu
    javascript file for top nav toggle function.

---
+++ scripts/top-nav.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
